bd9eea51ba1f6f0d83d3ef7dba12c65d33b21cf9
Eric Ito <eric.ito@gmail.com>
2014-04-05 23:38:25 -0700

    Initial Commit

---
+++ KonamiGo!/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KonamiGo!
+//
+//  Created by Eric Ito on 4/5/14.
+//  Copyright (c) 2014 Eric Ito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
